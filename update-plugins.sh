641c5c153efe65a326d27cf0a51845f7a25d7c94
falkTX <falktx@falktx.com>
2020-12-12 15:33:29 +0000

    Add script that fetches updated code

---
+++ update-plugins.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname "${0}")
+
+if [ ! -d ardour ]; then
+  git clone --depth=1 -b master git@github.com:Ardour/ardour.git
+fi
+
+pushd ardour
+git checkout .
+git pull
+popd
+
+# TODO use rsync
+rm -rf plugins
+cp -rv ardour/libs/plugins .
+rm -rf plugins/reasonablesynth.lv2
+git checkout plugins/*/Makefile
